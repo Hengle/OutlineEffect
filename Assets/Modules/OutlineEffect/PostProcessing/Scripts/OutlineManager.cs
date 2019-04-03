e5a162928b31ef98a80d32485011032abebdbb9d
ted <ted10401@gmail.com>
2019-04-03 13:44:28 +0800

    Add namespace.

--- Assets/Modules/OutlineEffect/PostProcessing/Scripts/OutlineManager.cs
+++ Assets/Modules/OutlineEffect/PostProcessing/Scripts/OutlineManager.cs
@@ -1,13 +1,12 @@
 ﻿using JSLCore;
-using UnityEngine;
 using System.Collections.Generic;
-using UnityEngine.Rendering;
-using UnityEngine.Rendering.PostProcessing;
 
-public class OutlineManager : Singleton<OutlineManager>
+namespace UnityEngine.Rendering.PostProcessing
 {
-    public readonly int COLOR_ID = Shader.PropertyToID("_Color");
-    private readonly Dictionary<OutlinePrepassType, Shader> m_prepassShaders = new Dictionary<OutlinePrepassType, Shader>
+    public class OutlineManager : Singleton<OutlineManager>
+    {
+        public readonly int COLOR_ID = Shader.PropertyToID("_Color");
+        private readonly Dictionary<OutlinePrepassType, Shader> m_prepassShaders = new Dictionary<OutlinePrepassType, Shader>
     {
         { OutlinePrepassType.SolidColor, Shader.Find("Outline/Prepass/SolidColor") },
         { OutlinePrepassType.SolidColorDepth, Shader.Find("Outline/Prepass/SolidColorDepth") },
@@ -15,114 +14,126 @@ public class OutlineManager : Singleton<OutlineManager>
         { OutlinePrepassType.AlphaDepth, Shader.Find("Outline/Prepass/AlphaDepth") },
     };
 
-    private List<OutlineData> m_outlineDatas = new List<OutlineData>();
-    private Outline m_outline;
-    private bool m_initialized;
+        private List<OutlineData> m_outlineDatas = new List<OutlineData>();
+        private bool m_initialized;
 
-    public Shader GetPrepassShader(OutlinePrepassType outlinePrepassType)
-    {
-        return m_prepassShaders[outlinePrepassType];
-    }
-
-    public void Register(GameObject parent, Color color, OutlinePrepassType outlinePrepassType)
-    {
-        Register(new OutlineData(parent, color, outlinePrepassType));
-    }
-
-    public void Register(OutlineData outlineData)
-    {
-        if (outlineData.renderers == null || outlineData.renderers.Length == 0)
+        public Camera camera
         {
-            return;
-        }
-
-        if (m_outlineDatas.Contains(outlineData))
-        {
-            return;
-        }
-
-        m_outlineDatas.Add(outlineData);
-        UpdateOutlineActive();
-    }
-
-    public void Unregister(OutlineData outlineData)
-    {
-        Unregister(outlineData.parent);
-    }
-
-    public void Unregister(GameObject parent)
-    {
-        for(int i = 0, count = m_outlineDatas.Count; i < count; i++)
-        {
-            if(m_outlineDatas[i].parent == parent)
+            get
             {
-                m_outlineDatas.RemoveAt(i);
-                UpdateOutlineActive();
-                break;
+                if (m_camera == null)
+                {
+                    m_camera = Camera.main;
+                }
+
+                return m_camera;
             }
         }
-    }
+        private Camera m_camera;
 
-    public void ExecuteCommandBuffer(CommandBuffer commandBuffer)
-    {
-        for (int i = 0, count = m_outlineDatas.Count; i < count; i++)
+        public PostProcessVolume postProcessVolume
         {
-            for (int j = 0; j < m_outlineDatas[i].renderers.Length; j++)
+            get
             {
-                commandBuffer.DrawRenderer(m_outlineDatas[i].renderers[j], m_outlineDatas[i].prepassMaterial);
+                if (m_postProcessVolume == null)
+                {
+                    if (camera != null)
+                    {
+                        m_postProcessVolume = camera.GetComponent<PostProcessVolume>();
+                    }
+                    else
+                    {
+                        m_postProcessVolume = GameObject.FindObjectOfType<PostProcessVolume>();
+                    }
+                }
+
+                return m_postProcessVolume;
             }
         }
-    }
+        private PostProcessVolume m_postProcessVolume;
 
-    private void UpdateOutlineActive()
-    {
-        GetOutline();
-
-        if (m_outline == null)
+        public Outline outline
         {
-            return;
+            get
+            {
+                if (postProcessVolume != null)
+                {
+                    m_outline = postProcessVolume.profile.GetSetting<Outline>();
+
+                    if (m_outline == null)
+                    {
+                        m_outline = postProcessVolume.profile.AddSettings<Outline>();
+                    }
+                }
+
+                return m_outline;
+            }
         }
-        
-        m_outline.active = m_outlineDatas.Count > 0;
-    }
+        private Outline m_outline;
 
-    private void GetOutline()
-    {
-        if(m_initialized)
+        public Shader GetPrepassShader(OutlinePrepassType outlinePrepassType)
         {
-            return;
+            return m_prepassShaders[outlinePrepassType];
         }
 
-        m_initialized = true;
-
-        //Find Outline Settings from Main Camera
-        if (m_outline == null)
+        public void Register(GameObject parent, Color color, OutlinePrepassType outlinePrepassType)
         {
-            Camera mainCamera = Camera.main;
-            if (mainCamera == null)
+            Register(new OutlineData(parent, color, outlinePrepassType));
+        }
+
+        public void Register(OutlineData outlineData)
+        {
+            if (outlineData.renderers == null || outlineData.renderers.Length == 0)
             {
                 return;
             }
 
-            PostProcessVolume postProcessVolume = mainCamera.GetComponent<PostProcessVolume>();
-            if (postProcessVolume == null || postProcessVolume.sharedProfile == null)
+            if (m_outlineDatas.Contains(outlineData))
             {
                 return;
             }
 
-            m_outline = postProcessVolume.sharedProfile.GetSetting<Outline>();
+            m_outlineDatas.Add(outlineData);
+            UpdateOutlineActive();
         }
 
-        //Find Outline Settings from PostProcessVolume
-        if (m_outline == null)
+        public void Unregister(OutlineData outlineData)
         {
-            PostProcessVolume postProcessVolume = GameObject.FindObjectOfType<PostProcessVolume>();
-            if (postProcessVolume == null || postProcessVolume.sharedProfile == null)
+            Unregister(outlineData.parent);
+        }
+
+        public void Unregister(GameObject parent)
+        {
+            for (int i = 0, count = m_outlineDatas.Count; i < count; i++)
+            {
+                if (m_outlineDatas[i].parent == parent)
+                {
+                    m_outlineDatas.RemoveAt(i);
+                    UpdateOutlineActive();
+                    break;
+                }
+            }
+        }
+
+        public void ExecuteCommandBuffer(CommandBuffer commandBuffer)
+        {
+            for (int i = 0, count = m_outlineDatas.Count; i < count; i++)
+            {
+                for (int j = 0; j < m_outlineDatas[i].renderers.Length; j++)
+                {
+                    commandBuffer.DrawRenderer(m_outlineDatas[i].renderers[j], m_outlineDatas[i].prepassMaterial);
+                }
+            }
+        }
+
+        private void UpdateOutlineActive()
+        {
+            if (outline == null)
             {
                 return;
             }
 
-            m_outline = postProcessVolume.sharedProfile.GetSetting<Outline>();
+            outline.active = m_outlineDatas.Count > 0;
         }
     }
 }
